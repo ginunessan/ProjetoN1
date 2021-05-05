a3bf782d8d076691394c27f5ee06ca8de15f5d60
Giovanna Nunes <ginunessan@gmail.com>
2021-05-05 20:50:46 -0300

    Classe Monstro
    Criação da classe Monstro e sus atributos

--- ProjetoN1/src/main/java/ProjetoN1/Elemento.java
+++ ProjetoN1/src/main/java/ProjetoN1/Elemento.java
@@ -1,14 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package ProjetoN1;
 
-/**
- *
- * @author Samsung
- */
+
 public class Elemento {
+    
+    private String nome;
+    private int vantagem;
+    private int desvantagem;
     
 }
---
+++ ProjetoN1/src/main/java/ProjetoN1/Monstro.java
@@ -0,0 +1,10 @@
+
+package ProjetoN1;
+
+
+public class Monstro {
+    
+    private String nome;
+    private Elemento elemento;
+    
+}
