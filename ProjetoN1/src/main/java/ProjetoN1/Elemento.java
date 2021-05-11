c8801d4e698de71ebb34721b4440fe4114eb10b6
Giovanna Nunes <ginunessan@gmail.com>
2021-05-11 15:48:03 -0300

    Criação Elementos :fire:
    Elaboração dos métodos get e set da classe e seu construtor. Criação dos elementos do jogo.

--- ProjetoN1/src/main/java/ProjetoN1/Elemento.java
+++ ProjetoN1/src/main/java/ProjetoN1/Elemento.java
@@ -4,10 +4,45 @@ package ProjetoN1;
 
 public class Elemento {
     
-    private String nome;
-    private int vantagem;
-    private int desvantagem;
+    private String elemento;
+    
+
+    public Elemento(String elemento) {
+        this.elemento = elemento;
+        
+    }
+
+    public String getElemento() {
+        return elemento;
+    }
+
+    public void setElemento(String elemento) {
+        this.elemento = elemento;
+    }
+
+    
+    
+    public void Fogo(String elemento){
+    
+        this.elemento = "Fogo";
+        
+    }
+    
+    public void Agua(String elemento){
+    
+        this.elemento = "Agua";
+        
+    }
+    
+    public void Grama(String elemento){
+    
+        this.elemento = "Grama";
+        
+    }
+    
     
 }
+
+
 
 
