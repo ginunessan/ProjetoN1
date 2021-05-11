2b170fde4d523d9da80cbf55d80431ff8c823dbb
Giovanna Nunes <ginunessan@gmail.com>
2021-05-11 15:50:23 -0300

    Criação Golpes
    Criação dos golpes presentes no jogo e valor dos atributos da classe, mais seu construtor e seus métodos get e set.

--- ProjetoN1/src/main/java/ProjetoN1/Golpe.java
+++ ProjetoN1/src/main/java/ProjetoN1/Golpe.java
@@ -7,5 +7,86 @@ public class Golpe {
     private String nome;
     private int poder;
     private Elemento elemento;
+
+    public Golpe(String nome, int poder, Elemento elemento) {
+        this.nome = nome;
+        this.poder = poder;
+        this.elemento = elemento;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getPoder() {
+        return poder;
+    }
+
+    public void setPoder(int poder) {
+        this.poder = poder;
+    }
+
+    public Elemento getElemento() {
+        return elemento;
+    }
+
+    public void setElemento(Elemento elemento) {
+        this.elemento = elemento;
+    }
+
+    public void TempestadeDeFogo (String nome, int poder, Elemento elemento){
+        this.nome = "Tempestade de Fogo";
+        this.poder = 5;
+}
+    public void ExplosaoSolar (String nome, int poder, Elemento elemento){
+        this.nome = "Explosao Solar";
+        this.poder = 7;
+}
+    public void Tsuname (String nome, int poder, Elemento elemento){
+        this.nome = "Tsuname";
+        this.poder = 2;
+}
+    public void AtaqueGelo (String nome, int poder, Elemento elemento){
+        this.nome = "Ataque de Gelo";
+        this.poder = 4;
+}
+    public void BolaDeFolhas (String nome, int poder, Elemento elemento){
+        this.nome = "Bola de Folhas";
+        this.poder = 3;
+}
+    public void Mordida (String nome, int poder, Elemento elemento){
+        this.nome = "Mordida";
+        this.poder = 2;
+}
+    public void LancaChamas (String nome, int poder, Elemento elemento){
+        this.nome = "Lança Chamas";
+        this.poder = 8;
+}
+    public void Vulcanismo (String nome, int poder, Elemento elemento){
+        this.nome = "Vulcanismo";
+        this.poder = 9;
+}
+    public void CanhaoAgua (String nome, int poder, Elemento elemento){
+        this.nome = "Canhao de Agua";
+        this.poder = 2;
+}
+    public void ExplosaoAgua (String nome, int poder, Elemento elemento){
+        this.nome = "Explosao de Agua";
+        this.poder = 6;
+}
+    public void PoderVentos (String nome, int poder, Elemento elemento){
+        this.nome = "Poder dos Ventos";
+        this.poder = 4;
+}
+    public void RajadaGrama (String nome, int poder, Elemento elemento){
+        this.nome = "Rajada de Grama";
+        this.poder = 5;
+}
+    
+    
     
 }
