fd395d3f07ffe56da95b93cca4c053a07b932d63
bn-pessoa <77862902+bn-pessoa@users.noreply.github.com>
2021-05-12 02:30:23 -0300

    Add files via upload

--- ProjetoN1/src/main/java/ProjetoN1/Main.java
+++ ProjetoN1/src/main/java/ProjetoN1/Main.java
@@ -1,38 +1,70 @@
-
-package ProjetoN1;
-
 import java.util.Scanner;
+import java.lang.Math;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
 
 public class Main {
+	
+	public static void main(String[] args) throws FileNotFoundException {			
+		
+		Scanner leitura = new Scanner (System.in);
+		System.out.println("=======================================");
+		System.out.println("\n     JOGO A BATALHA DOS DEUSES     \n");
+		System.out.println("=======================================");
+		System.out.println("Bem vindo! Digite para começar: ");
+		System.out.println("1 - Batalha entre players");
+		System.out.println("2 - Sair");
+		
+		int tipoJogo = leitura.nextInt(); 
+		
+		if(tipoJogo == 1){
 
-    public static void main(String[] args) {
-        
-        int player1[] = new int[3];
-        int player2[] = new int[3];
-        
-        
-        
-        System.out.println("JOGADOR 1");
-        System.out.println("Digite o número dos monstros ecolhidos:");
-        Scanner entrada = new Scanner(System.in);
-        
-        for(int i=0; i<3; i++){
-        player1[i] = entrada.nextInt();
-        }
-        
-        System.out.println("JOGADOR 2");
-        System.out.println("Digite o número dos monstros ecolhidos:");
-        
-        for(int i=0; i<3; i++){
-        player2[i] = entrada.nextInt();
-        }
-        
-        
-        
-        
-        
-        
-        
-    }
-    
+			Scanner players = new Scanner(new FileReader("criaPlayers.txt")).useDelimiter("\\||\\n");
+
+			String nome1 = players.next();
+			int quantidade = players.nextInt();				
+			Deuses[] listaDeuses1 = new Deuses[quantidade];
+			for (int i = 0; i < quantidade; i++) {
+				String nomePok = players.next();
+				String tipo = players.next();
+				int hp = players.nextInt();
+				Ataque[] listaAtk = new Ataque[4];
+				for (int j = 0; j < 4; j++) {
+					String nomeAtk = players.next();
+					int dano = players.nextInt();
+					listaAtk[j] = new Ataque(nomeAtk, dano);
+				}	
+				listaDeuses1[i] = new Deuses(nomePok, tipo, hp, listaAtk);
+			}
+			Players jogador1 = new Players (nome1, listaDeuses1);
+			
+			
+			String nome2 = players.next();
+			quantidade = players.nextInt();				
+			Deuses[] listaDeuses2 = new Deuses[quantidade];
+			for (int i = 0; i < quantidade; i++) {
+				String nomePok = players.next();
+				String tipo = players.next();
+				int hp = players.nextInt();
+				Ataque[] listaAtk = new Ataque[4];
+				for (int j = 0; j < 4; j++) {
+					String nomeAtk = players.next();
+					int dano = players.nextInt();
+					listaAtk[j] = new Ataque(nomeAtk, dano);
+				}	
+				listaDeuses2[i] = new Deuses(nomePok, tipo, hp, listaAtk);
+			}
+			Players jogador2 = new Players (nome2, listaDeuses2);
+
+			Batalha comum = new Batalha();
+			comum.batalhaComum(jogador1, jogador2);			
+		
+		}
+		else {
+			System.out.println("Obrigado por jogar!");
+		}
+		leitura.close();
+	}
+
 }
