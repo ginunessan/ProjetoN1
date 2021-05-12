7225e13187123c517270224021e7acd33657dcb5
Giovanna Nunes <ginunessan@gmail.com>
2021-05-12 00:40:35 -0300

    Escolha de Monstros
    Neste commit o usuario escolhe o numero referente ao monstro desejado(ainda não foi determinado) e as três escolhas são alocadas em um vetor.

--- ProjetoN1/src/main/java/ProjetoN1/Main.java
+++ ProjetoN1/src/main/java/ProjetoN1/Main.java
@@ -1,9 +1,37 @@
 
 package ProjetoN1;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
+        
+        int player1[] = new int[3];
+        int player2[] = new int[3];
+        
+        
+        
+        System.out.println("JOGADOR 1");
+        System.out.println("Digite o número dos monstros ecolhidos:");
+        Scanner entrada = new Scanner(System.in);
+        
+        for(int i=0; i<3; i++){
+        player1[i] = entrada.nextInt();
+        }
+        
+        System.out.println("JOGADOR 2");
+        System.out.println("Digite o número dos monstros ecolhidos:");
+        
+        for(int i=0; i<3; i++){
+        player2[i] = entrada.nextInt();
+        }
+        
+        
+        
+        
+        
+        
         
     }
     
