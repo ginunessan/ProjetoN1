4ab84376b9b1e2e21846af6f17e337aff43c09b6
Giovanna Nunes <ginunessan@gmail.com>
2021-05-11 23:32:50 -0300

    Associação da classe Elemento com classe Golpe
    Houve a associação da classe Elemento com a classe Golpe nos seus atributos neste commit.

--- ProjetoN1/src/main/java/ProjetoN1/Golpe.java
+++ ProjetoN1/src/main/java/ProjetoN1/Golpe.java
@@ -41,50 +41,62 @@ public class Golpe {
     public void TempestadeDeFogo (String nome, int poder, Elemento elemento){
         this.nome = "Tempestade de Fogo";
         this.poder = 5;
+        elemento.getFogo();
 }
     public void ExplosaoSolar (String nome, int poder, Elemento elemento){
         this.nome = "Explosao Solar";
         this.poder = 7;
+        elemento.getFogo();
 }
     public void Tsuname (String nome, int poder, Elemento elemento){
         this.nome = "Tsuname";
         this.poder = 2;
+        elemento.getAgua();
 }
     public void AtaqueGelo (String nome, int poder, Elemento elemento){
         this.nome = "Ataque de Gelo";
         this.poder = 4;
+        elemento.getAgua();
 }
     public void BolaDeFolhas (String nome, int poder, Elemento elemento){
         this.nome = "Bola de Folhas";
         this.poder = 3;
+        elemento.getGrama();
 }
     public void Mordida (String nome, int poder, Elemento elemento){
         this.nome = "Mordida";
         this.poder = 2;
+        elemento.getGrama();
 }
     public void LancaChamas (String nome, int poder, Elemento elemento){
-        this.nome = "Lança Chamas";
+        this.nome = "Lanca Chamas";
         this.poder = 8;
+        elemento.getFogo();
 }
     public void Vulcanismo (String nome, int poder, Elemento elemento){
         this.nome = "Vulcanismo";
         this.poder = 9;
+        elemento.getFogo();
 }
     public void CanhaoAgua (String nome, int poder, Elemento elemento){
         this.nome = "Canhao de Agua";
         this.poder = 2;
+        elemento.getAgua();
 }
     public void ExplosaoAgua (String nome, int poder, Elemento elemento){
         this.nome = "Explosao de Agua";
         this.poder = 6;
+        elemento.getAgua();
 }
     public void PoderVentos (String nome, int poder, Elemento elemento){
         this.nome = "Poder dos Ventos";
         this.poder = 4;
+        elemento.getGrama();
 }
     public void RajadaGrama (String nome, int poder, Elemento elemento){
         this.nome = "Rajada de Grama";
         this.poder = 5;
+        elemento.getGrama();
 }
     
     
