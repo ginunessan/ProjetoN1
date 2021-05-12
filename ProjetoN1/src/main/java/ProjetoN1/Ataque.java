5ac0814afe346a50d7cb9e797d1e67da50dc7f1f
bn-pessoa <77862902+bn-pessoa@users.noreply.github.com>
2021-05-12 02:27:38 -0300

    Add files via upload

--- ProjetoN1/src/main/java/ProjetoN1/Ataque.java
+++ ProjetoN1/src/main/java/ProjetoN1/Ataque.java
@@ -1,6 +1,15 @@
-
-package ProjetoN1;
-
 public class Ataque {
-    
+	private String nome;
+	private int forca;
+	
+	public Ataque(String nome, int dano){
+		this.nome = nome;
+		this.forca = dano;
+	}
+	public int getDano(){
+		return forca;
+	}
+	public String getNome(){
+		return nome;
+	}
 }
