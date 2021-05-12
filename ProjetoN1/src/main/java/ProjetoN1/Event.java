8e39e7d95868e81a71e9871a42c0a3cb3e2a2c0f
bn-pessoa <77862902+bn-pessoa@users.noreply.github.com>
2021-05-12 02:38:34 -0300

    Add files via upload

---
+++ ProjetoN1/src/main/java/ProjetoN1/Event.java
@@ -0,0 +1,15 @@
+
+public abstract class Event {
+	private long evtTime;
+	public Event(long eventTime) {
+		evtTime = eventTime;
+	}
+	public boolean ready() {
+		return System.currentTimeMillis() >= evtTime;
+	}
+	abstract public void action();
+	abstract public int prioridade();
+	abstract public boolean playersDerrotado();
+	abstract public boolean playersFugiu();
+	abstract public boolean playersTrocou();
+}
